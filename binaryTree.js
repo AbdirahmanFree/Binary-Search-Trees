a16dba673e2f5dd92e36338ed0d3dfb37e3cfba8
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-10-12 16:00:07 -0400

    built build tree and tree definition

--- binaryTree.js
+++ binaryTree.js
@@ -1,3 +1,21 @@
+
+class Tree {
+    constructor(array){
+        this.root = this.buildTree(array)
+
+    }
+
+    buildTree(){
+        
+    }
+}
+
+
+
+
+
+
+
 class Node {
     constructor(data, leftChild = null, rightChild = null){
         this.data = data
